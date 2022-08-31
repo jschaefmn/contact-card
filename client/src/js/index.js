74a58b8b91807235bceda1605f49b7afdbf7427b
Jake Schaefbauer <jschaefmn@gmail.com>
2022-08-31 09:43:39 -0500

    finihsed lesson 19.2.6

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,18 @@
 import "./form.js";
 import "./submit.js";
+import { Tooltip, Toast, Popover } from "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "../css/index.css";
+
+// Import images
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+// Add images on load
+window.addEventListener('load', function () {
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
