3db70d1898b492b0ea2791dcfd8639ae3657d009
Jake Schaefbauer <jschaefmn@gmail.com>
2022-08-31 23:24:30 -0500

    finished 19.3.2

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,6 +2,7 @@ import "./form.js";
 import "./submit.js";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initdb } from './database';
 
 import "../css/index.css";
 
@@ -12,6 +13,7 @@ import Dog from '../images/dog.png';
 
 // Add images on load
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
